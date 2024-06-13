316f197c2878131c429fb5a8dac1d574609b5f7f
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-06-12 22:44:34 -0300

    Criando um gerador de arquivos

--- src/ConsultaCEP.java
+++ src/ConsultaCEP.java
@@ -12,17 +12,17 @@ public class ConsultaCEP {
 
         URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
 
-        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(endereco)
                 .build();
-        HttpResponse<String> response = null;
+
         try {
-            response = HttpClient
-                    .newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
+        HttpResponse<String> response = HttpClient
+                .newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (Exception e) {
             throw new RuntimeException("Não consegui obter o endereço a partir desse CEP: " + cep);
         }
-        return new Gson().fromJson(response.body(), Endereco.class);
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,61 @@
+/*
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+
         ConsultaCEP newCep = new ConsultaCEP();
-        Endereco novoEndereco = newCep.buscaEndereco("1");
-        System.out.println(novoEndereco);
+        Scanner leitrua = new Scanner(System.in);
+
+        System.out.println("Informe o N° do CEP:");
+        var cep = leitrua.nextLine();
+
+        try {
+            Endereco novoEndereco = newCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+    }
+}
+*/
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        ConsultaCEP newCep = new ConsultaCEP();
+        Scanner leitura = new Scanner(System.in);
+        String cep;
+
+        do {
+            System.out.println("Informe o N° do CEP:");
+            cep = leitura.nextLine();
+
+            try {
+                if (cep.length() != 8) {
+                    System.out.println("CEP inválido. O CEP deve ter 8 dígitos.");
+                    continue; // Pula para a próxima iteração do loop
+                }
+
+                Endereco novoEndereco = newCep.buscaEndereco(cep);
+                System.out.println(novoEndereco);
+                GeradorDeArquivo gerador = new GeradorDeArquivo();
+                gerador.salvaJson(novoEndereco);
+
+            } catch (RuntimeException | IOException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (cep.length() != 8);
+
+        System.out.println("Finalizando a aplicação");
+        leitura.close();
     }
 }
